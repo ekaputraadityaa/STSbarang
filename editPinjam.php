6f856628d6a570dfa2caf0410893acbf10cd1f93
ekaputraaditya <148317874+ekaputraadityaa@users.noreply.github.com>
2024-03-15 12:44:44 +0700

    Update editPinjam.php

--- editPinjam.php
+++ editPinjam.php
@@ -1,58 +1,55 @@
 <?php
-require_once 'koneksi.php';
-if (isset($_POST["edit"])) {
-    $id = $_POST["id"];
-    $idBtn = $_POST["edit"];
-    $data = editpinjam("peminjaman", $id);
-} else {
-    null;
-}
+  require_once("koneksi.php");
+  $data=Editdata("peminjaman",$_GET['id']);
 ?>
 
 <!doctype html>
 <html lang="en">
   <head>
-    <!-- Required meta tags -->
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css" integrity="sha384-xOolHFLEh07PJGoPkLv1IbcEPTNtaed2xpHsD9ESMhqIYd0nLMwNLD69Npy4HI+N" crossorigin="anonymous">
 
-    <!-- Bootstrap CSS -->
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
-
-    <title>Hello, world!</title>
+    <title>Insert Barang</title>
   </head>
   <body>
-  <div class="container mt-5">
-        <h4 class="text-center">Ubah Peminjaman</h4>
-        <form class="" method="post">
-            <input type="hidden" name="id" value="<?= $pinjam['id']; ?>">
-            <div class="form-group">
-                <label for="tgl_pinjam">Username</label>
-                <input type="text" value="<?= $pinjam['tgl_pinjam']; ?>" class="form-control" name="username" id="tgl_pinjam" placeholder="masukan tanggal pinjam" required>
-            </div>
-            <div class="form-group">
-                <label for="tgl_kembali">Kode Barang</label>
-                <input type="text" value="<?= $pinjam['tgl_kembali']; ?>" class="form-control" name="kode_brg" id="tgl_kembali" placeholder="masukan tanggal kembali" required>
-            </div>
-            <div class="form-group">
-                <label for="no_identitas">Nama Barang</label>
-                <input type="text" value="<?= $pinjam['no_identitas']; ?>" class="form-control" name="namabrng" id="no_identitas" placeholder="masukan no identitas" required>
-            </div>
-            <div class="form-group">
-                <label for="kode_barang">Jumlah</label>
-                <input type="text" value="<?= $pinjam['kode_barang']; ?>" class="form-control" name="jumlahh" id="kode_barang" placeholder="masukan kode barang" required>
-            </div>
-            <div class="form-group">
-                <label for="jumlah">Tanggal Kembali</label>
-                <input type="date" value="<?= $pinjam['jumlah']; ?>" class="form-control" name="tanggal_kembali" id="jumlah" placeholder="masukan jumlah" required>
-            </div>
-            <button type="submit" name="submit" class="btn btn-primary" style="float: right;">Ubah</button>
-        </form>
+    <div class="container">
+    <h1> Edit Peminjaman </h1>
+    <?php while($peminjaman= mysqli_fetch_array($data)): ?>
+    <form action="update2.php" method="post">
+      <input type="hidden" class="form-control" name="id" value="<?= $peminjaman['id'];?>">
+  <div class="form-group">
+    <label>Tgl_Pinjam</label>
+    <input type="date" class="form-control" name="tgl_pinjam">
+  </div>
+  <div class="form-group">
+    <label>Tgl_Kembali</label>
+    <input type="date" class="form-control" name="tgl_kembali">
+  </div>
+  <div class="form-group">
+    <label>No_Identitas</label>
+    <input type="text" class="form-control" name="no_identitas">
+  </div>
+  <div class="form-group">
+    <label>Kode Barang</label>
+    <input type="text" class="form-control" name="kode_brg">
+  </div>
+  <div class="form-group">
+    <label>Jumlah</label>
+    <input type="text" class="form-control" name="jumlah">
+  </div>
+  <button type="submit" name="update" class="btn btn-primary">Submit</button>
+</form>
+    <?php endwhile; ?>
     </div>
-    <!-- Optional JavaScript -->
-    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
-    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.12.9/dist/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/jquery@3.5.1/dist/jquery.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-Fy6S3B9q64WdZWQUiU+q4/2Lc9npb8tCaSX9FK7E8HnRr0Jz8D6OP9dO5Vg3Q9ct" crossorigin="anonymous"></script>
+
+    <!-- Option 2: Separate Popper and Bootstrap JS -->
+    <!--
+    <script src="https://cdn.jsdelivr.net/npm/jquery@3.5.1/dist/jquery.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/js/bootstrap.min.js" integrity="sha384-+sLIOodYLS7CIrQpBjl+C7nPvqq+FbNUBDunl/OZv93DB7Ln/533i8e/mZXLi/P+" crossorigin="anonymous"></script>
+    -->
   </body>
 </html>
