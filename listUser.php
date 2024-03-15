58f1594ce861114ca092b7d7ace6a492b9cc0423
ekaputraaditya <148317874+ekaputraadityaa@users.noreply.github.com>
2024-03-15 15:53:07 +0700

    Update listUser.php

--- listUser.php
+++ listUser.php
@@ -38,7 +38,7 @@ $number = 0
         </nav>
     </div>
     <div class ="container">
-    <h1>List Barang</h1> 
+    <h1>List user</h1> 
     <table class="table w-100 mt-2" id="table">
     <thead>
                     <tr>
