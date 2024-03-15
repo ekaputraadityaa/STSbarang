640d9da4aad994f3478cd29aab1923ac05801481
ekaputraaditya <148317874+ekaputraadityaa@users.noreply.github.com>
2024-03-15 12:44:07 +0700

    Update update.php

--- update.php
+++ update.php
@@ -1,15 +1,14 @@
 <?php
-    require_once("koneksi.php");
-    $id = $_POST['id'];
-    $tgl_pinjam = $_POST['tgl_pinjam'];
-    $tgl_Kembali = $_POST['tgl_kembali'];
-    $no_identitas = $_POST['no_identitas'];
-    $kode_brg = $_POST['kode_brg'];
-    $jumlah = $_POST['jumlah'];
+require_once("koneksi.php");
+$id = $_POST['id'];
+$kodeBrg = $_POST['kodeBrg'];
+$namaBrg= $_POST['namaBrg'];
+$kategori = $_POST['kategori'];
+$merk = $_POST['merk'];
+$jumlah = $_POST['jumlah'];
+$sql = updateBarang("barang", $id, $kodeBrg, $namaBrg, $kategori, $merk, $jumlah);
 
-    $sql2=update("barang", $id, $kode, $nama, $kategori, $merk, $jumlah);
-    if ($sql2) {
-        header("location:listBarang.php");
-    }
-    
+if($sql){
+    header("location:listBarang.php");
+}
 ?>
