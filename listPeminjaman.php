1921992a3f9aa5b2d64a96ae4159143d009453ee
ekaputraaditya <148317874+ekaputraadityaa@users.noreply.github.com>
2024-03-15 12:41:41 +0700

    Update listPeminjaman.php

--- listPeminjaman.php
+++ listPeminjaman.php
@@ -32,13 +32,13 @@ $number = 0
                         <a class="dropdown-item" href="listUser.php">User</a>
                     </div>
                 </div>
-                <h3 class="text-light ml-4">Peminjaman Barang</h3>
+                <h3 class="text-light ml-4">list Peminjaman</h3>
             </div>
             <a href="login.php" class="btn btn-outline-success my-2 my-sm-0">LOGOUT</a>
         </nav>
     </div>
     <div class ="container">
-    <h1>List Barang</h1>   <button><a href="add1.php">ADD</a></button>
+    <h1>List Peminjaman</h1>   <button><a href="add1.php">ADD</a></button>
     <table class="table w-100 mt-2" id="table">
     <thead>
                     <tr>
@@ -75,13 +75,7 @@ $number = 0
                                 <?= $data['jumlah']; ?>
                             </td>
                                 <td>
-                                <form class="btn btn-warning" action="" method="post">
-                                        <input type="hidden" value="<?= $data['id']; ?>" name="id">
-                                        <button type="button" name="edit" value="<?= $data['id']; ?>"
-                                            class='btn btn-primary' data-toggle='modal'
-                                            data-target='#staticBackdrop'>Edit</button>
-                                    </form>    
-                                <?php echo " <a href='javascript:hapusData(".$data['id'].")'><button class='btn btn-danger'>Hapus</button></a>"; ?></td>
+                                <?php echo "<a href='editPinjam.php?id=$data[id_pinjam]'><button class='btn btn-warning'>Edit</button></a> <a href='javascript:hapusData(".$data['id_pinjam'].")'><button class='btn btn-danger'>Hapus</button></a>"; ?></td>
                         </tr>
                     <?php endforeach; ?>
                 </tbody>
@@ -94,7 +88,6 @@ $number = 0
         }
       }
     </script>
-
     <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/popper.js@1.12.9/dist/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
